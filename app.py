bc31387b5d8644545f32ac9390eb08508dabe952
lhtadityachourasiya <adityac@leewayhertz.com>
2025-02-13 18:58:28 +0530

    Method 2 added

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 #Find largest element in array
 
+# Method 2
 arr = [2, -8, 97, 12, 44]
 
 def find_largest(arr: list) -> float:
     le = arr[0]
-    for i in range(len(arr)):
-        if arr[i] > le:
-            le = arr[i]
+    for i in arr:
+        if i > le:
+            le = i
     return le
 
 print("The largest element is:", find_largest(arr))
